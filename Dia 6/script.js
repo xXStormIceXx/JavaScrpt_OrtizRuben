586cf8c23736b21a2450a525931e0e5e2f99466b
Ruben <ruben.ortiz.db9@gmail.com>
2024-04-22 07:18:15 -0500

    feat: :sparkles: Secorrigieron errores y se finalizo el trabajo

--- Dia 6/script.js
+++ Dia 6/script.js
@@ -68,3 +68,113 @@ const coche1 = new Coche("Toyota", "Corolla", 2022, 4);
 coche1.MostrarDetalles();
 
 /*================================ Clase "Cuenta Bancaria" ===================================*/
+
+class CuentaBancaria {
+    contructor(NumeroCuenta, SaldoInicial) {
+        this.NumeroCuenta = NumeroCuenta;
+        this.Saldo = SaldoInicial;
+    }
+
+    depositar(monto) {
+        this.Saldo += monto;
+        console.log(`Deposito realizado. Saldo actual: ${this.Saldo}`);
+    }
+
+    retirar(monto) {
+        if (monto <= this.Saldo){
+            this.Saldo -= monto;
+            console.log(`Retiro realizado. Saldo actual: ${this.Saldo}`);
+        } else {
+            console.log("Fondos Insuficientes.");
+        }
+    }
+}
+
+const cuenta1 = new CuentaBancaria("12345", 1000);
+cuenta1.depositar(500);
+cuenta1.retirar(200);
+
+/*============================== Clases "Forma, Circulo y Triangulo" ===========================*/
+
+class Forma {
+    CalcularArea() {
+        console.log ("areade la forma (base):0");
+    }
+}
+
+class Circulo extends Forma {
+    constructor(radio) {
+        super();
+        this.radio = radio;
+    }
+
+    CalcularArea() {
+        const area = Math.PI * this.radio ** 2;
+        console.log(`Area del circulo: ${area.toFixed(2)}`);
+    }
+}
+
+class Triangulo extends Forma {
+    constructor(base, altura) {
+        super();
+        this.base = base;
+        this.altura = altura;
+    }
+}
+
+const circulo1 = new Circulo(5);
+circulo1.CalcularArea();
+
+/*=============================== Encadenamiento de Constructores ============================= */
+
+class A {
+    constructor(arg){
+        this.arg = arg;
+    }
+}
+
+class B extends A {
+    constructor(arg) {
+        super(arg);
+    }
+}
+
+class C extends B {
+    constructor(arg) {
+        super(arg);
+    }
+}
+
+const instanceC = new C("Hola, Soy un Argumento");
+console.log(instanceC.arg);
+
+/*=============================== Herencia y Poliformismo de Estudiantes ======================== */
+
+class Student{
+    constructor(name) {
+        this.name = name;
+    }
+
+    pass() {
+        return true;
+    }
+}
+
+class NSStudent extends Student {
+    contructor(name) {
+        super(name);
+    }
+
+    pass () {
+        return false;
+    }
+}
+
+const regularStudent = new Student("Juan");
+const nsStudent = new NSStudent("Maria");
+
+const studentList = [regularStudent, nsStudent];
+
+for (const Student of studentList) {
+    console.log(`${Student.name} Pasa el Año: ${Student.pass()}`);
+}
