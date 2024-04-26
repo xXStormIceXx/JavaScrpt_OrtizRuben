4438595f06266aede34235ac83311ab042d107b8
Ruben Dario <ruben.ortiz.db9@gmail.com>
2024-04-26 08:42:11 -0500

    feat: :sparkles: Creacion de carpetas y visual de POKEDEX

---
+++ Dia 11/js/script.js
@@ -0,0 +1,8 @@
+const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+const form = document.querySelector('.form');
+const input = document.querySelector('.input__search');
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
